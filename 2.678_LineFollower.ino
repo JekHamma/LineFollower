e9912a55276bfc0522971c2aad0798db397880e7
JekHamma <118999242+JekHamma@users.noreply.github.com>
2023-12-06 08:39:49 -0500

    WITH PID

--- 2.678_LineFollower.ino
+++ 2.678_LineFollower.ino
@@ -11,12 +11,24 @@ const int LR = A0;
 const int MR = A1;
 const int RR = A2;
 
+float left;
+float middle;
+float right;
+
+float Kp = 2;
+float Ki = .5;
+float I_error = 0;
+float Kd = .01;
+float Der = 0;;
+float previous_error = 0;
+
 float speedL = 50;
 float speedR = 50;
 
 int tol = 20;
 
 int speed_min = 10;
+int max_speed = 50;
 
 void setup(){
   pinMode(PWMA , OUTPUT);
@@ -36,9 +48,15 @@ void setup(){
 }
 
 void loop(){
-read_data();
-steer();
-drive(speedL,speedR);
+  steer();
+  drive(speedL, speedR);
+}
+
+float compute_PID(float error){
+  I_error = I_error + .01*error;
+  Der = (error - previous_error)/.01;
+  previous_error = error;
+  return Kp*error + Ki*I_error + Kd*Der;
 }
 
 
@@ -55,47 +73,44 @@ void motorWrite(int spd, int pin_IN1 , int pin_IN2 , int pin_PWM){
   analogWrite(pin_PWM , abs(spd));
 }
 
-
 void drive(int speedL, int speedR){
   //Takes both speed inputs and passes them to the motorWrite function with correct parameters assuming drive A is hooked to the left motor.
   motorWrite(speedL, AIN1, AIN2, PWMA);
   motorWrite(speedR, BIN1, BIN2, PWMB);
   
-  }
+}
 
 float read_data(){
-  float left = analogRead(LR);
-  float middle = analogRead(MR);
-  float right = analogRead(RR);
-  Serial.print(left);
-  Serial.print(", ");
-  Serial.print(middle);
-  Serial.print(", ");
-  Serial.println(right);
+  left = analogRead(LR);
+  middle = analogRead(MR);
+  right = analogRead(RR);
+  // Serial.print(left);
+  // Serial.print(", ");
+  // Serial.print(middle);
+  // Serial.print(", ");
+  // Serial.println(right);
+ 
  }
 
 
 
- void steer(){
-  float left = analogRead(LR);
-  float middle = analogRead(MR);
-  float right = analogRead(RR);
-  int error = abs(left-right);
-  
+void steer(){
+  read_data();
+  float error = abs(left-right);
+  float change = compute_PID(error);
+
   if (abs(left-right) > tol){
     if (left < right){
-      speedR = speedR - .02*error;}
+      speedR = speedR - change;}
     if (left > right){
-      speedL = speedL - .02*error;}
+      speedL = speedL - change;}
   }
   else{
-    speedL = 50;
-    speedR = 50;}
+    speedL = max_speed;
+    speedR = max_speed;}
 
   Serial.print(speedL);
   Serial.print("    ");
   Serial.println(speedR);
 }
-
-
 
